ee61ffbda50e38b7767d98855710b364052280e8
AHMET İNCEKARA <ahmet3@hotmail.com.tr>
2024-12-03 01:00:11 +0300

    30. Derse kadar yaıldı

---
+++ MvcProjeKampi/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
+        }
+    }
+
+
+}
