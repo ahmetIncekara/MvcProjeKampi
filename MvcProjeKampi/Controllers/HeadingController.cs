491cf2037602257ca265130a46e6eba392bd9709
AHMET İNCEKARA <ahmet3@hotmail.com.tr>
2024-12-06 00:53:59 +0300

    51. Derse kadar yapıldı

--- MvcProjeKampi/Controllers/HeadingController.cs
+++ MvcProjeKampi/Controllers/HeadingController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concretes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,42 @@ namespace MvcProjeKampi.Controllers
 {
     public class HeadingController : Controller
     {
-        // GET: Heading
+        HeadingManager hm = new HeadingManager(new EFHeadingDal());
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
+        WriterManager wm = new WriterManager(new EFWriterDal());
+
         public ActionResult Index()
         {
+            var headingValues = hm.GetList();
+            return View(headingValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddHeading()
+        {
+            List<SelectListItem> categoryValues = (from x in cm.GetList()
+                                                    select new SelectListItem
+                                                    {
+                                                        Text = x.CategoryName,
+                                                        Value = x.CategoryID.ToString()
+                                                    }).ToList();
+            List<SelectListItem> writerValues = (from x in wm.GetList()
+                                                 select new SelectListItem
+                                                 {
+                                                     Text = x.WriterName + " " + x.WriterSurName,
+                                                     Value = x.WriterID.ToString()
+                                                 }).ToList();
+            ViewBag.categoryValues = categoryValues;
+            ViewBag.writerValues = writerValues;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddHeading(Heading p)
+        {
+            p.HeadingDate = DateTime.Now;
+            hm.HeadingAdd(p);
+            return RedirectToAction("Index");
         }
     }
 }
